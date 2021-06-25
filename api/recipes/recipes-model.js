e18fd37817ffd53c42f4b1214442b623ebf78bd4
Gabriel Henton <gabehenton@gmail.com>
2021-06-25 11:23:36 -0700

    MVP complete

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,69 +1,49 @@
 const db = require("../../data/db-config.js")
 
-function getRecipeById(recipe_id) {
-    return db("recipes as r")
-        .select("r.*", "s.*", "ifrs.ingredient_quantity", "i.*")
-        .where("r.recipe_id", recipe_id)
-        .leftJoin("steps as s", "r.recipe_id", "s.recipe_id")
-        .leftJoin("ingredients_for_recipe_steps as ifrs", "s.step_id", "ifrs.step_id")
+
+async function getRecipeById(recipe_id) {
+    const allRecipes = await db("recipes as r")
+        .join("steps as s", "s.recipe_id", "r.recipe_id")
+        .leftJoin("ingredients_for_recipe_steps as ifrs", "ifrs.step_id", "s.step_id")
         .leftJoin("ingredients as i", "i.ingredient_id", "ifrs.ingredient_id")
+        .select(
+            "r.recipe_id",
+            "r.recipe_name",
+            "s.step_id",
+            "s.step_number",
+            "s.step_instructions",
+            "i.ingredient_id",
+            "i.ingredient_name",
+            "ifrs.ingredient_quantity"
+        )
+        .where("r.recipe_id", recipe_id)
         .orderBy("s.step_number", "asc")
-        .then(data => {
-            if (!data || Object.keys(data).length === 0) {
-                return []
-            }
-            let arr = []
-            data.map(step => {
 
-                if (!step.recipe_id) {
-                    return arr
-                }
-                let exists = false
-                arr.map(steps => {
-                    if (steps.step_id == step.step_id) {
-                        exists = true
-                        if (step.ingredient_quantity) {
-                            let ingredient = {
-                                ingredient_id: step.ingredient_id,
-                                ingredient_name: step.ingredient_name,
-                                ingredient_quantity: step.ingredient_quantity
-                            }
-                            return steps.ingredients.push(ingredient)
-                        }
-                        else {
-                            return steps
-                        }
-                    }
-                    return steps
-                })
-                if (!exists) {
-                    let newStep = {
-                        step_id: step.step_id,
-                        step_number: step.step_number,
-                        step_instructions: step.step_instructions
-                    }
-                    if (step.quantity) {
-                        newStep.ingredients = []
-                        let ingredient = {
-                            ingredient_id: step.ingredient_id,
-                            ingredient_name: step.ingredient_name,
-                            ingredient_quantity: step.ingredient_quantity
-                        }
-                        newStep.ingredients.push(ingredient)
-
-                    } else {
-                        newStep.ingredients = []
-                    }
-                    arr.push(newStep)
-                }
+    const recipeObject = {};
+    allRecipes.forEach(row => {
+        if (!recipeObject.recipe_id || !recipeObject.recipe_name) {
+            recipeObject.recipe_id = row.recipe_id;
+            recipeObject.recipe_name = row.recipe_name;
+            recipeObject.steps = []
+        }
+        if (!recipeObject.steps[row.step_number - 1] && row.step_id) {
+            recipeObject.steps.push({
+                "step_id": row.step_id,
+                "step_number": row.step_number,
+                "step_instructions": row.step_instructions,
+                "ingredients": [],
             })
-            let finalResult = {
-                recipe_id: data[0].recipe_id,
-                recipe_name: data[0].recipe_name,
-                steps: arr
-            }
-            return finalResult
-        })
-}
+        }
+        if (row.ingredient_id) {
+            recipeObject.steps[row.step_number - 1].ingredients.push({
+                "ingredient_id": row.ingredient_id,
+                "ingredient_name": row.ingredient_name,
+                "ingredient_quantity": row.ingredient_quantity
+            })
+        }
+    })
+
+    return recipeObject;
+};
 
 module.exports = { getRecipeById }
